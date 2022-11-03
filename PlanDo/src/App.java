93c295238666410bbe2bd56b56537456a6178518
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-11-03 01:50:00 +0000

    feat - added option to complete task and remove it from todo list

--- PlanDo/src/App.java
+++ PlanDo/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         TaskManager taskManager = new TaskManager();
-        taskManager.addTask("oi");
         System.out.println(taskManager.readTaskList()); 
+        taskManager.completeTask(1);
     }
 }
--- PlanDo/src/TaskManager.java
+++ PlanDo/src/TaskManager.java
@@ -6,12 +6,13 @@ import java.io.FileWriter;
 public class TaskManager {
 
     // file path
-    private File file = new File("tasks/tasks.txt");
+    private File todos = new File("PlanDo/tasks/todos.txt");
+    private File completed = new File("PlanDo/tasks/completed.txt");
 
     // Adds task to tasks/tasks.txt file
     public void addTask(String taskName) {
         try {
-            FileWriter writer = new FileWriter(file, true);
+            FileWriter writer = new FileWriter(todos, true);
 
             writer.write("task_name: " + taskName + "\n");
             writer.write("id: " + genId() + "\n\n");
@@ -35,7 +36,7 @@ public class TaskManager {
 
     public String readTaskList() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedReader reader = new BufferedReader(new FileReader(todos));
 
             //String that will be used to check on while
             String st;
@@ -51,7 +52,7 @@ public class TaskManager {
                 data += st + "\n";
                 flag = true;
             }
-            if (flag == false) data = "Lista vazia";
+            if (!flag) data = "Lista vazia";
 
             reader.close();
 
@@ -63,7 +64,70 @@ public class TaskManager {
         }
     }
 
-    public void completeTask() {
+    public void completeTask(int taskId) {        
+        boolean flag = false;
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(todos));
+            FileWriter writer = new FileWriter(completed, true);
+
+            String taskName = reader.readLine();
+            String id;
+
+            while (taskName != null) {
+                id = reader.readLine();
+                /* Comparable integer that is the 2 last characters of the
+                 * "id" line, minus the whitespaces before and after the line
+                 */
+                int compId = Integer.parseInt(id.substring(id.length() - 2).trim());
+
+                //if current id is equal to the passed id, write it to completed.txt
+                if (id.contains("id: ") && taskId == compId) {
+                    flag = true;
+
+                    writer.write(taskName + "\n");
+
+                    //Remove from todos.txt
+                    removeTask(taskName, id);
+                }
+                taskName = reader.readLine();
+                taskName = reader.readLine();
+            }
+        
+            if (!flag) System.out.println("Could not find task by the provided task id");
+            else System.out.println("Task completed!");
+
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public boolean removeTask(String name, String id) {
+        try {
+            File tempFile = new File("PlanDo/tasks/temp.txt");
+            FileWriter writer = new FileWriter(tempFile);
+            BufferedReader reader = new BufferedReader(new FileReader(todos));
+            String currentLine;
+            String nextLine;
+
+            while((currentLine = reader.readLine()) != null) {
+                //If current line equals to task name or next line is equal to the id, write to temp
+                if(currentLine.equals(name) && (nextLine = reader.readLine()).equals(id)) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+            writer.close(); 
+            reader.close(); 
+
+            
+            boolean successful = tempFile.renameTo(todos);
+
+            return successful;
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
     }
 }
